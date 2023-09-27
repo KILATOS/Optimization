35549b5cda2b6109e66f283a98aab761c4a8b1c5
онстантин ильчитский <kilatos@yandex.ru>
2023-09-27 13:25:12 +0300

    removing all parameters into static final variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,40 +16,42 @@ import util.ParableMethod;
 public class Main {
     private static double prevValue = Double.MAX_VALUE;
     private static double middleValue = Double.MAX_VALUE;
-    private static final double epsilon = 0.00001;
-
+    private static final double epsilon = 0.0001;
+    private static final double step = 0.01;
+    private static final float leftX = -10;
+    private static final float rightX = 10;
 
 
     public static void main(String[] args) {
         XYSeries series = new XYSeries("f(x)");
 
 
-        for (float i = -10; i < 10; i += 0.01) {
+        for (float i = leftX; i < rightX; i += step) {
             double f = Math.pow(i, 3) + (2 / (Math.sin(i) + Math.pow(i, 2)));
             series.add(i, f);
             if (f > middleValue && middleValue < prevValue && prevValue != Double.MAX_VALUE && Math.abs(f - middleValue) < 1) {
                 System.out.println("//////НАХОДИМ МИНИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////");
-                System.out.println(GoldenSection.findMin(i - 0.02, i, epsilon));
-                System.out.println("НАчало интервала: " + (i-0.02) + " Конец интервала: " + i);
+                System.out.println(GoldenSection.findMin(i - step - step, i, epsilon));
+                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
                 System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
                 System.out.println("Количество итераций: " + GoldenSection.minOperationsCounter);
                 System.out.println("//////////////////////////////////////////////////////////////");
                 System.out.println();
                 System.out.println("//////НАХОДИМ МИНИМУМ МЕТОДОМ КВАДРАТИЧНОЙ ИНТЕРПОЛЯЦИИ///////");
-                System.out.println(ParableMethod.parabolicMethod(i - 0.02, i,epsilon));
-                System.out.println("НАчало интервала: " + (i-0.02) + " Конец интервала: " + i);
+                System.out.println(ParableMethod.parabolicMethod(i - step - step, i, epsilon));
+                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
                 System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
-                System.out.println("Количество итераций: " +ParableMethod.minOperationsCounter);
+                System.out.println("Количество итераций: " + ParableMethod.minOperationsCounter);
                 System.out.println("//////////////////////////////////////////////////////////////");
                 System.out.println();
                 //System.out.println(i + " Минимум");
             }
             if (f < middleValue && middleValue > prevValue && prevValue != Double.MAX_VALUE && Math.abs(f - middleValue) < 1) {
                 System.out.println("//////НАХОДИМ МАКСИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////");
-                System.out.println(GoldenSection.findMax(i - 0.02, i, epsilon));
-                System.out.println("НАчало интервала: " + (i-0.02) + " Конец интервала: " + i);
+                System.out.println(GoldenSection.findMax(i - step - step, i, epsilon));
+                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
                 System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
-                System.out.println("Количество итераций: " +GoldenSection.maxOperationsCounter);
+                System.out.println("Количество итераций: " + GoldenSection.maxOperationsCounter);
                 System.out.println("///////////////////////////////////////////////////////////////");
                 System.out.println();
                 //System.out.println(ParableMethod.parabolicMethod(i - 0.02, i,0.001));
