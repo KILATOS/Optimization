6ad564c3e2cc0491ae70068af3125c43882ac451
онстантин ильчитский <kilatos@yandex.ru>
2023-09-27 14:14:47 +0300

    adding all info in file with auto opening

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,14 @@ import util.Function;
 import util.GoldenSection;
 import util.ParableMethod;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     private static double prevValue = Double.MAX_VALUE;
     private static double middleValue = Double.MAX_VALUE;
@@ -22,47 +30,55 @@ public class Main {
     private static final float rightX = 10;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         XYSeries series = new XYSeries("f(x)");
+        File file = new File("D:/newFile.txt");
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
+
+
 
 
         for (float i = leftX; i < rightX; i += step) {
             double f = Math.pow(i, 3) + (2 / (Math.sin(i) + Math.pow(i, 2)));
             series.add(i, f);
             if (f > middleValue && middleValue < prevValue && prevValue != Double.MAX_VALUE && Math.abs(f - middleValue) < 1) {
-                System.out.println("//////НАХОДИМ МИНИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////");
-                System.out.println(GoldenSection.findMin(i - step - step, i, epsilon));
-                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
-                System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
-                System.out.println("Количество итераций: " + GoldenSection.minOperationsCounter);
-                System.out.println("//////////////////////////////////////////////////////////////");
-                System.out.println();
-                System.out.println("//////НАХОДИМ МИНИМУМ МЕТОДОМ КВАДРАТИЧНОЙ ИНТЕРПОЛЯЦИИ///////");
-                System.out.println(ParableMethod.parabolicMethod(i - step - step, i, epsilon));
-                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
-                System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
-                System.out.println("Количество итераций: " + ParableMethod.minOperationsCounter);
-                System.out.println("//////////////////////////////////////////////////////////////");
-                System.out.println();
+                bufferedWriter.write("//////НАХОДИМ МИНИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////\n");
+                bufferedWriter.write(Double.valueOf(GoldenSection.findMin(i - step - step, i, epsilon)).toString() + "\n");
+                bufferedWriter.write("Начало интервала: " + (i - step - step) + " Конец интервала: " + i + "\n");
+                bufferedWriter.write("ТОЧНОСТЬ РАВНА = " + epsilon +"\n");
+                bufferedWriter.write("Количество итераций: " + GoldenSection.minOperationsCounter +"\n");
+                bufferedWriter.write("//////////////////////////////////////////////////////////////\n");
+                bufferedWriter.write("\n");
+
+                bufferedWriter.write("//////НАХОДИМ МИНИМУМ МЕТОДОМ КВАДРАТИЧНОЙ ИНТЕРПОЛЯЦИИ///////\n");
+                bufferedWriter.write(Double.valueOf(ParableMethod.parabolicMethod(i - step - step, i, epsilon)).toString() + "\n");
+                bufferedWriter.write("Начало интервала: " + (i - step - step) + " Конец интервала: " + i  +"\n");
+                bufferedWriter.write("ТОЧНОСТЬ РАВНА = " + epsilon + "\n");
+                bufferedWriter.write("Количество итераций: " + ParableMethod.minOperationsCounter + "\n");
+                bufferedWriter.write("//////////////////////////////////////////////////////////////\n");
+                bufferedWriter.write("\n");
                 //System.out.println(i + " Минимум");
             }
             if (f < middleValue && middleValue > prevValue && prevValue != Double.MAX_VALUE && Math.abs(f - middleValue) < 1) {
-                System.out.println("//////НАХОДИМ МАКСИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////");
-                System.out.println(GoldenSection.findMax(i - step - step, i, epsilon));
-                System.out.println("Начало интервала: " + (i - step - step) + " Конец интервала: " + i);
-                System.out.println("ТОЧНОСТЬ РАВНА = " + epsilon);
-                System.out.println("Количество итераций: " + GoldenSection.maxOperationsCounter);
-                System.out.println("///////////////////////////////////////////////////////////////");
-                System.out.println();
-                //System.out.println(ParableMethod.parabolicMethod(i - 0.02, i,0.001));
-                //System.out.println(i + " Максимум");
+                bufferedWriter.write("//////НАХОДИМ МАКСИМУМ ПРИ ПОМОЩИ МЕТОДА ЗОЛОТОГО СЕЧЕНИЯ//////"+ "\n");
+                bufferedWriter.write(Double.valueOf(GoldenSection.findMax(i - step - step, i, epsilon)).toString() + "\n");
+                bufferedWriter.write("Начало интервала: " + (i - step - step) + " Конец интервала: " + i + "\n");
+                bufferedWriter.write("ТОЧНОСТЬ РАВНА = " + epsilon + "\n");
+                bufferedWriter.write("Количество итераций: " + GoldenSection.maxOperationsCounter + "\n");
+                bufferedWriter.write("///////////////////////////////////////////////////////////////\n");
+                bufferedWriter.write("\n");
             }
             prevValue = middleValue;
             middleValue = f;
         }
+        } catch (IOException e){ e.printStackTrace();}
 
        /* System.out.println(GS.findMin(0, 10, 0.001) + " Min");
         System.out.println(GS.findMax(-1, 0, 0.001) + " Max");*/
+
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd /d D: && .\\newFile.txt\"");
+        Thread.sleep(100);
+        Runtime.getRuntime().exec("taskkill /f /im cmd.exe") ;
 
 
         XYDataset xyDataset = new XYSeriesCollection(series);
